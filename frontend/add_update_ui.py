7510019476732a747779e98edba36ebf8711bc2c
ayushagrawal4 <agrawal.ayushh786@gmail.com>
2025-04-05 14:52:23 +0530

    removed local host and tried streamlit url

--- frontend/add_update_ui.py
+++ frontend/add_update_ui.py
@@ -2,7 +2,7 @@ import streamlit as st
 from datetime import datetime
 import requests
 
-API_URL = "http://localhost:8000"
+API_URL = "https://ayush-agrawal-expense-tracking-system.streamlit.app"
 
 
 def add_update_tab():
